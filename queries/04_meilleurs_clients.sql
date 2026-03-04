e9a74a8368bbe0c97618f5df24efc2ea10096657
Alexis <sayasith.alexis@gmail.com>
2026-03-04 13:28:54 +0100

    Panier moyen par client

--- queries/04_meilleurs_clients.sql
+++ queries/04_meilleurs_clients.sql
@@ -8,3 +8,18 @@ JOIN details_commandes dc ON c.id_commande = dc.id_commande
 WHERE c.statut = 'livré'
 GROUP BY cl.id_client, cl.nom, cl.ville
 ORDER BY total_depense DESC;
+
+--- PANIER MOYEN PAR CLIENT 
+SELECT 
+    c.nom,
+    COUNT(DISTINCT co.id_commande) AS nb_commandes,
+    ROUND(SUM(dc.prix_unitaire * dc.quantite), 2) AS ca_total,
+    ROUND(SUM(dc.prix_unitaire * dc.quantite) / 
+        COUNT(DISTINCT co.id_commande), 2) AS panier_moyen
+FROM clients c
+JOIN commandes co ON c.id_client = co.id_client
+JOIN details_commandes dc ON co.id_commande = dc.id_commande
+WHERE co.statut = 'livré'
+GROUP BY c.id_client, c.nom
+ORDER BY panier_moyen DESC
+LIMIT 10;
